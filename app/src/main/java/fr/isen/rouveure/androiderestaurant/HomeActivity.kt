7d6a191b517f071243ce3a409f14c24bf4047560
ROUVEURE <milena.rouveure@isen.yncrea.fr>
2022-01-24 17:52:53 +0100

    add toast

--- app/src/main/java/fr/isen/rouveure/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/rouveure/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,33 @@ package fr.isen.rouveure.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        var button2 = findViewById(R.id.button2) as Button
+
+        button2.setOnClickListener {
+            // toast on button click event
+            Toast.makeText(this, "Vous avez choisie les entrées.", Toast.LENGTH_LONG).show()
+        }
+        var button3 = findViewById(R.id.button3) as Button
+
+        button3.setOnClickListener {
+            // toast on button click event
+            Toast.makeText(this, "Vous avez choisie les plats.", Toast.LENGTH_LONG).show()
+        }
+
+        var button4 = findViewById(R.id.button4) as Button
+
+        button4.setOnClickListener {
+            // toast in button click event
+            Toast.makeText(this, "Vous avez choisie les deserts.", Toast.LENGTH_LONG).show()
+        }
     }
 }
